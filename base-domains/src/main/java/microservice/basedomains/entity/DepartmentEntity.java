6bae58743e3032c42c7356f48d64cf76f920caca
azilzor <athirsonarceus@gmail.com>
2023-03-30 13:22:11 -0300

    feat: creating all mongodb documents

---
+++ base-domains/src/main/java/microservice/basedomains/entity/DepartmentEntity.java
@@ -0,0 +1,25 @@
+package microservice.basedomains.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "departments")
+public class DepartmentEntity {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String code;
+    @CreatedDate
+    private String createdAt;
+    @LastModifiedDate
+    private String updatedAt;
+}
---
+++ base-domains/src/main/java/microservice/basedomains/entity/EmployeeEntity.java
@@ -0,0 +1,26 @@
+package microservice.basedomains.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "employees")
+public class EmployeeEntity {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String departmentId;
+    @CreatedDate
+    private String createdAt;
+    @LastModifiedDate
+    private String updatedAt;
+}
