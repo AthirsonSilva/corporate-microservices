ca797b1120028cca69b748e9349ae0264b2e803e
azilzor <athirsonarceus@gmail.com>
2023-03-30 14:38:45 -0300

    feat: adding mongodb support to microservices

--- department-service/src/main/java/microservice/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/microservice/departmentservice/DepartmentServiceApplication.java
@@ -2,8 +2,12 @@ package microservice.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableMongoRepositories
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
